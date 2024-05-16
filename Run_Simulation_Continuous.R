16d7c171eb384c378e46a93e00a787fbaba57765
Melody Owen <melodyowen@Melodys-MBP.med.yale.internal>
2024-05-16 14:58:13 -0400

    adding table of all sim parameters

--- Run_Simulation_Continuous.R
+++ Run_Simulation_Continuous.R
@@ -2,7 +2,30 @@ source("./DataGeneration/Generate_Data_2_Cont_Outcomes.R")
 source("./RequiredPackages.R")
 
 # Table of all Parameters ------------------------------------------------------
+sim_betas <- tibble(betas = c(paste("0.3 0.7"),
+                              paste("0.7 0.7")))
+sim_K <- tibble(K = c(15))
+sim_m <- tibble(m = c(80))
+sim_rho0 <- tibble(rho0 = c(paste("0.01 0.1"),
+                            paste("0.1 0.1"),
+                            paste("0.1 0.01")))
+sim_rho1 <- tibble(rho1 = c(0.025))
+sim_rho2 <- tibble(rho2 = c(0.2, 0.5))
 
+simParameters <- expand.grid(K = c(15),
+                             m = c(80),
+                             betas = c(paste("0.3 0.7"),
+                                       paste("0.7 0.7")),
+                             rho0 = c(paste("0.01 0.1"),
+                                      paste("0.1 0.1"),
+                                      paste("0.1 0.01")),
+                             rho1 = c(0.025),
+                             rho2 = c(0.2, 0.5)
+                             ) %>%
+  separate(betas, sep = " ", into = c("beta1", "beta2")) %>%
+  separate(rho0, sep = " ", into = c("rho01", "rho02"))
+
+# Run power calculations on all true parameters --------------------------------
 
 
 # Generate Continuous Datasets -------------------------------------------------
