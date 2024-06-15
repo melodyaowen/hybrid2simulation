48a0ad62370bc1062c47ffb247d0fcdd8ecc24ed
Melody Owen <melodyowen@Melodys-MBP.lan>
2024-06-15 16:38:42 -0400

    updating shiny app

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -28,7 +28,52 @@ ui <- fluidPage(
 
   p("This ShinyApp lets the user calculate the number of clusters in the treatment group ($K$), cluster size ($m$), or statistical power ($\\pi$) from the user's desired input parameters. Calculations are done using the R package `crt2power`."),
 
-  # Third section (m) -------------
+  shinyjs::useShinyjs(),
+  shinyjs::extendShinyjs(text = "shinyjs.refresh_page = function() { location.reload(); }", functions = "refresh_page"),
+  actionButton("refresh", "Refresh Application"),
+
+  # UI 1 (K) -------------
+  p("Calculate Power: probability of correctly rejecting the null hypothesis", style = "color:blue; font-size:25px"),
+
+  p("Input Parameters", style = "font-size:20px"),
+
+  # Inputs at the top
+  fluidRow(
+    column(3,
+           textInput("K_power", "$K_1$: Number of clusters in treatment group", value = ""),
+           textInput("beta1_power", "$\\beta_1$: Effect for Y1", value = ""),
+           textInput("beta2_power", "$\\beta_2$: Effect for Y2", value = ""),
+           textInput("alpha_power", "$\\alpha$: Type I error", value = "")
+    ),
+    column(3,
+           textInput("m_power", "$m$: Number of subjects in each cluster", value = ""),
+           textInput("varY1_power", "Var$(Y_1)$: Total variance of outcome 1", value = ""),
+           textInput("varY2_power", "Var$(Y_2)$: Total variance of outcome 2", value = ""),
+           textInput("rho1_power", "$\\rho_1^{(1,2)}$: Inter-subject between-endpoint ICC", value = "")
+    ),
+    column(3,
+           textInput("r_power", "$r = K_2 / K_1$: Treatment allocation ratio", value = ""),
+           textInput("rho01_power", "$\\rho_0^{(1)}$: ICC for outcome 1", value = ""),
+           textInput("rho02_power", "$\\rho_0^{(2)}$: ICC for outcome 2", value = ""),
+           textInput("rho2_power", "$\\rho_2^{(1,2)}$: Intra-subject between-endpoint ICC", value = "")
+    )
+  ),
+
+  # Action button to trigger the calculation
+  fluidRow(
+    column(12,
+           actionButton("calcButton1", "Calculate")
+    )
+  ),
+
+  # Output: Bargraph
+  fluidRow(
+    column(12,
+           plotOutput("bargraph1")
+    )
+  ),
+
+  # UI 2 (K) -------------
   p("Calculate $K$: number of clusters in treatment group", style = "color:blue; font-size:25px"),
 
   p("Input Parameters", style = "font-size:20px"),
@@ -36,22 +81,22 @@ ui <- fluidPage(
   # Inputs at the top
   fluidRow(
     column(3,
-           textInput("power_K", "Power:", value = ""),
-           textInput("beta1_K", "Effect for Y1:", value = ""),
-           textInput("beta2_K", "Effect for Y2:", value = ""),
-           textInput("alpha_K", "Type I error:", value = "")
+           textInput("power_K", "$\\pi$: Statistical power", value = ""),
+           textInput("beta1_K", "$\\beta_1$: Effect for Y1", value = ""),
+           textInput("beta2_K", "$\\beta_2$: Effect for Y2", value = ""),
+           textInput("alpha_K", "$\\alpha$: Type I error", value = "")
     ),
     column(3,
-           textInput("m_K", "m:", value = ""),
-           textInput("varY1_K", "Total variance of Y1:", value = ""),
-           textInput("varY2_K", "Total variance of Y2:", value = ""),
-           textInput("rho1_K", "Inter-subject between-endpoint ICC:", value = "")
+           textInput("m_K", "$m$: Number of subjects in each cluster", value = ""),
+           textInput("varY1_K", "Var$(Y_1)$: Total variance of outcome 1", value = ""),
+           textInput("varY2_K", "Var$(Y_2)$: Total variance of outcome 2", value = ""),
+           textInput("rho1_K", "$\\rho_1^{(1,2)}$: Inter-subject between-endpoint ICC", value = "")
     ),
     column(3,
-           textInput("r_K", "Treatment allocation ratio:", value = ""),
-           textInput("rho01_K", "ICC for Y1:", value = ""),
-           textInput("rho02_K", "ICC for Y2:", value = ""),
-           textInput("rho2_K", "Intra-subject between-endpoint ICC:", value = "")
+           textInput("r_K", "$r = K_2 / K_1$: Treatment allocation ratio", value = ""),
+           textInput("rho01_K", "$\\rho_0^{(1)}$: ICC for outcome 1", value = ""),
+           textInput("rho02_K", "$\\rho_0^{(2)}$: ICC for outcome 2", value = ""),
+           textInput("rho2_K", "$\\rho_2^{(1,2)}$: Intra-subject between-endpoint ICC", value = "")
     )
   ),
 
@@ -69,7 +114,7 @@ ui <- fluidPage(
     )
   ),
 
-  # Third section (m) -------------
+  # UI 3 (m) -------------
   p("Calculate $m$: number of individuals per cluster", style = "color:blue; font-size:25px"),
 
   p("Input Parameters", style = "font-size:20px"),
@@ -77,22 +122,22 @@ ui <- fluidPage(
   # Inputs at the top
   fluidRow(
     column(3,
-           textInput("power_m", "Power:", value = ""),
-           textInput("beta1_m", "Effect for Y1:", value = ""),
-           textInput("beta2_m", "Effect for Y2:", value = ""),
-           textInput("alpha_m", "Type I error:", value = "")
+           textInput("power_m", "$\\pi$: Statistical power", value = ""),
+           textInput("beta1_m", "$\\beta_1$: Effect for Y1", value = ""),
+           textInput("beta2_m", "$\\beta_2$: Effect for Y2", value = ""),
+           textInput("alpha_m", "$\\alpha$: Type I error", value = "")
     ),
     column(3,
-           textInput("K_m", "K (# clusters in treatment group):", value = ""),
-           textInput("varY1_m", "Total variance of Y1:", value = ""),
-           textInput("varY2_m", "Total variance of Y2:", value = ""),
-           textInput("rho1_m", "Inter-subject between-endpoint ICC:", value = "")
+           textInput("K_m", "$K_1$: Number of clusters in treatment group", value = ""),
+           textInput("varY1_m", "Var$(Y_1)$: Total variance of outcome 1", value = ""),
+           textInput("varY2_m", "Var$(Y_2)$: Total variance of outcome 2", value = ""),
+           textInput("rho1_m", "$\\rho_1^{(1,2)}$: Inter-subject between-endpoint ICC", value = "")
     ),
     column(3,
-           textInput("r_m", "Treatment allocation ratio:", value = ""),
-           textInput("rho01_m", "ICC for Y1:", value = ""),
-           textInput("rho02_m", "ICC for Y2:", value = ""),
-           textInput("rho2_m", "Intra-subject between-endpoint ICC:", value = "")
+           textInput("r_m", "$r = K_2 / K_1$: Treatment allocation ratio", value = ""),
+           textInput("rho01_m", "$\\rho_0^{(1)}$: ICC for outcome 1", value = ""),
+           textInput("rho02_m", "$\\rho_0^{(2)}$: ICC for outcome 2", value = ""),
+           textInput("rho2_m", "$\\rho_2^{(1,2)}$: Intra-subject between-endpoint ICC", value = "")
     )
   ),
 
@@ -114,7 +159,146 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
 
-  # Section results 2 (K) ---------------
+  observeEvent(input$refresh, {
+    shinyjs::js$refresh_page()
+  })
+
+# Server 1 (Power) ---------------
+  # Reactive expression to run the functions and create the data frame
+  results1 <- eventReactive(input$calcButton1, {
+    # Get user inputs
+    r_input <- as.numeric(input$r_power)
+    m_input <- as.numeric(input$m_power)
+    K_input <- as.numeric(input$K_power)
+    alpha_input <- as.numeric(input$alpha_power)
+    beta1_input <- as.numeric(input$beta1_power)
+    beta2_input <- as.numeric(input$beta2_power)
+    varY1_input <- as.numeric(input$varY1_power)
+    varY2_input <- as.numeric(input$varY2_power)
+    rho01_input <- as.numeric(input$rho01_power)
+    rho02_input <- as.numeric(input$rho02_power)
+    rho1_input <- as.numeric(input$rho1_power)
+    rho2_input <- as.numeric(input$rho2_power)
+
+    r_input <- 1
+    m_input <- 300
+    K_input <- 15
+    alpha_input <- 0.05
+    beta1_input <- 0.1
+    beta2_input <- 0.1
+    varY1_input <- 0.23
+    varY2_input <- 0.25
+    rho01_input <- 0.025
+    rho02_input <- 0.025
+    rho1_input <- 0.01
+    rho2_input <- 0.05
+
+    # Ensure inputs are not empty
+    if (!is.na(r_input) && !is.na(m_input) && !is.na(K_input) && !is.na(alpha_input) && !is.na(beta1_input) && !is.na(beta2_input) && !is.na(varY1_input) && !is.na(varY2_input) && !is.na(rho01_input) && !is.na(rho02_input) && !is.na(rho1_input) && !is.na(rho2_input)) {
+      power1_bonf <- calc_pwr_pval_adj(K = K_input, m = m_input,
+                                     alpha = alpha_input,
+                                     beta1 = beta1_input, beta2 = beta2_input,
+                                     varY1 = varY1_input, varY2 = varY2_input,
+                                     rho01 = rho01_input, rho02 = rho02_input,
+                                     rho2  = rho2_input, r = r_input)$`Final Power`[1]
+
+      power1_sidak <- calc_pwr_pval_adj(K = K_input, m = m_input,
+                                      alpha = alpha_input,
+                                      beta1 = beta1_input, beta2 = beta2_input,
+                                      varY1 = varY1_input, varY2 = varY2_input,
+                                      rho01 = rho01_input, rho02 = rho02_input,
+                                      rho2  = rho2_input, r = r_input)$`Final Power`[2]
+
+      power1_dap <- calc_pwr_pval_adj(K = K_input, m = m_input,
+                                alpha = alpha_input,
+                                beta1 = beta1_input, beta2 = beta2_input,
+                                varY1 = varY1_input, varY2 = varY2_input,
+                                rho01 = rho01_input, rho02 = rho02_input,
+                                rho2  = rho2_input, r = r_input)$`Final Power`[3]
+
+      power2 <- calc_pwr_comb_outcome(K = K_input, m = m_input,
+                                alpha = alpha_input, r = r_input,
+                                beta1 = beta1_input, beta2 = beta2_input,
+                                varY1 = varY1_input, varY2 = varY2_input,
+                                rho01 = rho01_input, rho02 = rho02_input,
+                                rho1 = rho1_input, rho2  = rho2_input)
+
+      power3 <- calc_pwr_single_1dftest(m = m_input, K = K_input,
+                                  alpha = alpha_input, r = r_input,
+                                  beta1 = beta1_input, beta2 = beta2_input,
+                                  varY1 = varY1_input, varY2 = varY2_input,
+                                  rho01 = rho01_input, rho02 = rho02_input,
+                                  rho1 = rho1_input, rho2  = rho2_input)
+
+      power4_F <- calc_pwr_disj_2dftest(m = m_input, K = K_input,
+                                  alpha = alpha_input, r = r_input,
+                                  beta1 = beta1_input, beta2 = beta2_input,
+                                  varY1 = varY1_input, varY2 = varY2_input,
+                                  rho01 = rho01_input, rho02 = rho02_input,
+                                  rho1 = rho1_input, rho2  = rho2_input,
+                                  dist = "F")
+
+      power4_Chi2 <- calc_pwr_disj_2dftest(m = m_input, K = K_input,
+                                     alpha = alpha_input, r = r_input,
+                                     beta1 = beta1_input, beta2 = beta2_input,
+                                     varY1 = varY1_input, varY2 = varY2_input,
+                                     rho01 = rho01_input, rho02 = rho02_input,
+                                     rho1 = rho1_input, rho2  = rho2_input,
+                                     dist = "Chi2")
+
+      power5_T <- calc_pwr_conj_test(m = m_input, K = K_input,
+                               alpha = alpha_input, r = r_input,
+                               beta1 = beta1_input, beta2 = beta2_input,
+                               varY1 = varY1_input, varY2 = varY2_input,
+                               rho01 = rho01_input, rho02 = rho02_input,
+                               rho1 = rho1_input, rho2  = rho2_input,
+                               dist = "T")
+
+      power5_MVN <- calc_pwr_conj_test(m = m_input, K = K_input,
+                                 alpha = alpha_input, r = r_input,
+                                 beta1 = beta1_input, beta2 = beta2_input,
+                                 varY1 = varY1_input, varY2 = varY2_input,
+                                 rho01 = rho01_input, rho02 = rho02_input,
+                                 rho1 = rho1_input, rho2  = rho2_input,
+                                 dist = "MVN")
+
+      # Create a data frame with the results
+      data.frame(
+        Function = c("P-Value Adj. (Bonferonni)", "P-Value Adj. (Sidak)",
+                     "P-Value Adj. (D/AP)", "Combined Outcomes",
+                     "Single Weighted", "Disjunctive F-dist",
+                     "Disjunctive Chi2", "Conjunctive T", "Conjunctive MVN"),
+        Value = c(power1_bonf, power1_sidak, power1_dap,
+                  power2, power3, power4_F, power4_Chi2,
+                  power5_T, power5_MVN),
+        Fill = c(1, 1, 1, 2, 3, 4, 4, 5, 5)
+      )
+    } else {
+      data.frame(
+        Function = character(0),
+        Value = numeric(0)
+      )
+    }
+  })
+
+  # Output 1 (m) -------------
+  output$bargraph1 <- renderPlot({
+    # Get the results
+    df <- results1()
+
+    # Check if the data frame is empty
+    if (nrow(df) > 0) {
+      # Create the bar graph using ggplot2
+      ggplot(df, aes(x = reorder(Function, Fill), y = Value)) +
+        geom_bar(stat = "identity", fill = 'purple') +
+        ylab("Power") +
+        xlab("Design Method") +
+        geom_text(aes(label = Value), vjust = -0.5, size = 4) +
+        ggtitle("Figure 1. Results for statistical power")
+    }
+  })
+
+# Server 2 (K) ---------------
   # Reactive expression to run the functions and create the data frame
   results2 <- eventReactive(input$calcButton2, {
     # Get user inputs
@@ -453,7 +637,7 @@ server <- function(input, output) {
     }
   })
 
-  # Output the bargraph 2 for K -------------
+# Output 2 (K) -------------
   output$bargraph2 <- renderPlot({
     # Get the results
     df <- results2()
@@ -473,7 +657,7 @@ server <- function(input, output) {
     }
   })
 
-  # Section results 3 (m) ---------------
+# Server 3 (m) ---------------
   # Reactive expression to run the functions and create the data frame
   results3 <- eventReactive(input$calcButton3, {
     # Get user inputs
@@ -589,7 +773,7 @@ server <- function(input, output) {
     }
   })
 
-  # Output the bargraph 3 for m -------------
+# Output 3 (m) -------------
   output$bargraph3 <- renderPlot({
     # Get the results
     df <- results3()
